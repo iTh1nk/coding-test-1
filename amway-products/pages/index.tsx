7924a200863801bb3d55695e7de7bffc0f3a58c2
iTh1nk <fnchao@hotmail.com>
2021-04-11 13:48:02 +0800

    finished

--- amway-products/pages/index.tsx
+++ amway-products/pages/index.tsx
@@ -4,32 +4,61 @@ const koneIniPrice = 3488.99;
 const konePromoPrice = 2588.99;
 const cartridgeIniPrice = 529.99;
 
+type Amway = {
+  level: string;
+  kone: number;
+  cartridge: number;
+  result: number;
+};
+
 const Home = (): ReactNode => {
   const [result, setResult] = useState<number>(0);
-  const [recipient, setRecipient] = useState<string>("");
+  const [resultArr, setArr] = useState<Array<Amway>>([]);
+  const [recipient, setRecipient] = useState<string>("None");
   const [kone, setKone] = useState<string>("0");
   const [cartridge, setCartridge] = useState<string>("0");
 
   const finalPriceOfKone = (kone: number): number => {
     if (kone >= 3) {
-      return koneIniPrice * 2 + konePromoPrice * (kone - 2);
+      return konePromoPrice * kone;
     }
-    if (kone) return 0;
+    if (recipient === "Associate") {
+      return koneIniPrice * kone * 0.95;
+    } else if (recipient === "Diamond") {
+      return koneIniPrice * kone * 0.8;
+    }
+    return 0;
   };
   const finalPriceOfCartridge = (cartridge: number): number => {
+    if (cartridge >= 3 && cartridge % 3 === 0) {
+      return (
+        (cartridgeIniPrice * (cartridge - (cartridge % 3)) * 2) / 3 +
+        cartridgeIniPrice * (cartridge % 3)
+      );
+    }
+    if (recipient === "Associate") {
+      return cartridgeIniPrice * cartridge * 0.95;
+    } else if (recipient === "Diamond") {
+      return cartridgeIniPrice * cartridge * 0.8;
+    }
     return 0;
   };
   const onSubmit = (): void => {
-    const konePrice: number = finalPriceOfKone(parseInt(kone));
+    const koneFinalPrice: number = finalPriceOfKone(parseInt(kone));
     const cartridgeFinalPrice: number = finalPriceOfCartridge(
       parseInt(cartridge)
     );
-    setResult(() => parseInt(kone) + parseInt(cartridge));
+    console.log(koneFinalPrice, cartridgeFinalPrice);
+    setResult((pre) => (pre = koneFinalPrice + cartridgeFinalPrice));
+    setArr([
+      ...resultArr,
+      { level: "hello", kone: 1, cartridge: 1, result: 2 },
+    ]);
   };
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Amway Products</h1>
+      <h1>Amway Business</h1>
       <div>
         <form
           onSubmit={(e) => {
@@ -56,14 +85,14 @@ const Home = (): ReactNode => {
           </div>
           <div style={{ marginBottom: "2em", marginTop: "2em" }}>
             <div style={{ marginBottom: "1em" }}>
-              <label>Input Kone Amounts: </label>
+              <label>Input Kone Amount: </label>
               <input
                 type="text"
                 onChange={(e) => setKone(() => e.target.value)}
               />
             </div>
             <div>
-              <label>Input Ironhide Cartridge Amounts: </label>
+              <label>Input Ironhide Cartridge Amount: </label>
               <input
                 type="text"
                 onChange={(e) => setCartridge(() => e.target.value)}
@@ -83,13 +112,18 @@ const Home = (): ReactNode => {
             </div>
             <h2 style={{ marginTop: ".5em", color: "green" }}>
               <label>Result: </label>
-              <span>${result}</span>
+              <span>${result.toFixed(2)}</span>
             </h2>
           </div>
           <div>
             <button
               style={{ marginRight: "2em", fontSize: "1.2em" }}
               type="reset"
+              onClick={() => {
+                setRecipient((pre) => (pre = "None"));
+                setKone((pre) => (pre = "0"));
+                setCartridge((pre) => (pre = "0"));
+              }}
             >
               Reset
             </button>
@@ -98,6 +132,32 @@ const Home = (): ReactNode => {
             </button>
           </div>
         </form>
+        <div style={{ marginTop: "2em" }}>
+          {resultArr.length === 0 ? (
+            <div>Results history will show here</div>
+          ) : (
+            <table style={{ margin: "auto" }}>
+              <thead>
+                <tr>
+                  <th>Level -- </th>
+                  <th>Kone -- </th>
+                  <th>Cartridge -- </th>
+                  <th>Result </th>
+                </tr>
+              </thead>
+              <tbody>
+                {resultArr?.map((item, idx) => (
+                  <tr key={idx}>
+                    <td>{item?.level}</td>
+                    <td>{item?.kone}</td>
+                    <td>{item?.cartridge}</td>
+                    <td>{item?.result}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
     </div>
   );
